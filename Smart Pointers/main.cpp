115d2287d1c33c3f82ce5fee371dd2c4a3d3dd6a
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2021-02-09 08:05:55 +0100

    no message

--- Smart Pointers/main.cpp
+++ Smart Pointers/main.cpp
@@ -2,30 +2,26 @@
 #include <string>
 #include <iostream>
 #include <vector>
-using String = std::string;  // using instead of typedef
+using String = std::string; // using instead of typedef
 
 class Planet
 {
 
 public:
-
     Planet() = default;
-    explicit Planet(String name): name_(name){};
-    ~Planet() { std::cout << "Planet Destructor: " << name_  << std::endl; };
+    explicit Planet(String name) : name_(name){};
+    ~Planet() { std::cout << "Planet Destructor: " << name_ << std::endl; };
 
     std::string name_ = "Courusant";
     void setName(std::string name) noexcept
     {
         name_ = name;
     };
-
-
 };
 using UpPlanet = std::unique_ptr<Planet>;
 using SpPlanet = std::shared_ptr<Planet>;
 void printPlanet(std::unique_ptr<Planet> planet);
 void printPlanetObserver(SpPlanet planet);
-
 
 int main(void)
 {
@@ -45,11 +41,11 @@ int main(void)
     planet = std::make_unique<Planet>(Planet());
     std::vector<UpPlanet> planets{};
     //planets.push_back(std::move(planet)); // ERROR. unique_ptr(const unique_ptr&) = delete;
-    planets.push_back(std::make_unique<Planet>(Planet("Tatooine")));  // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
-    planets.push_back(std::make_unique<Planet>(Planet("Dathomir")));  // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
-    planets.push_back(std::make_unique<Planet>(Planet("Naboo")));  // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
-    planets.push_back(std::make_unique<Planet>(Planet("Endor Moon")));  // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
-    for(uint8_t i = 0U; i < planets.size(); i++)
+    planets.push_back(std::make_unique<Planet>(Planet("Tatooine")));   // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
+    planets.push_back(std::make_unique<Planet>(Planet("Dathomir")));   // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
+    planets.push_back(std::make_unique<Planet>(Planet("Naboo")));      // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
+    planets.push_back(std::make_unique<Planet>(Planet("Endor Moon"))); // Se crea un planeta, se asigna a un unique_ptr y se elimina el planeta
+    for (uint8_t i = 0U; i < planets.size(); i++)
     {
         std::cout << planets[i].get()->name_ << std::endl;
     }
@@ -66,6 +62,5 @@ void printPlanet(std::unique_ptr<Planet> planet)
 void printPlanetObserver(std::shared_ptr<Planet> planet)
 {
     std::cout << "[Observer Function] Planet Name: " << planet.get()->name_ << std::endl;
-     std::cout << "[Observer Function] Planet Count: " << planet.use_count() << std::endl;
-    
+    std::cout << "[Observer Function] Planet Count: " << planet.use_count() << std::endl;
 }
